772b86211dd0f1db849b4ac9e8f8e55da250ca10
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2024-01-03 11:12:51 +0100

    2-digit least

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,18 +2,3 @@
 for i in range(0, 99):
     print('{:02d}, '.format(i), end='')
 print(99)
-
-
-
-
-
-
-
-
-
-
-#Write a program that prints numbers from 0 to 99.
-
-#Numbers must be separated by ,, followed by a space
-#Numbers should be printed in ascending order, with two digits
-#The last number should be followed by a new line
