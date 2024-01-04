9dc1337fc1f0b4710741bddb1fd0ef5d0d1a509d
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2024-01-04 22:50:01 +0100

    printing all functions

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     from hidden_4 import *
     funcs = dir()
     for i in range(0, len(funcs)):
-        if func[i][:2] != "__":
-            print("{:s}".format(func[i]))
+        if funcs[i][:2] != "__":
+            print("{:s}".format(funcs[i]))
