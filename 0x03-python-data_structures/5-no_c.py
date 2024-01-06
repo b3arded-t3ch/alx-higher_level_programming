de55c7d84e4586257394b784063dafa2ce5ba079
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2024-01-06 10:21:22 +0100

    no c and C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,10 +2,3 @@
 def no_c(my_string):
     new_string = ''.join(i for i in my_string if i != 'c' and i != 'C')
     return (new_string)
-
-
-   # new_string = ""
-   # for i in my_string:
-    #    if i != 'c' and i != 'C':
-     #       new_string = new_string + i
-   # return (new_string)
