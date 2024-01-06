a119d66b3ee568ae70dddd355fecea1b2b4b6e54
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2024-01-06 23:21:18 +0100

    delete at

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,8 @@
+def delete_at(my_list=[], idx=0):
+    if idx < 0 and idx > len(my_list):
+        return (my_list)
+    else:
+        for i in range(len(my_list)):
+            if i == idx:
+                del my_list[i]
+        return (my_list)
