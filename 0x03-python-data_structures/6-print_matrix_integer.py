87a1db1d9589172cce7e4a8cb6d843b841f440b1
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2024-01-07 09:28:35 +0100

    matrixing

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{}".format(matrix[i][j]), end=' ')
+    for i in matrix:
+        for j in i:
+            print("{}".format(j), end=' ' if j != i[-1] else '')
         print()
-   
-  # for row in matrix:
-   #     new_matrix = ' '.join(map(str, row))
-    #    print("{}".format(new_matrix))
