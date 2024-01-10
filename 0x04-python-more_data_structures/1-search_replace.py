71f76443b071c99482310272c41699b29d9b3f10
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2024-01-10 20:53:59 +0100

    search/replace cor.

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list:
+    if my_list is not None:
         new_list = my_list[:]
         for i in range(len(new_list)):
             if new_list[i] == search:
