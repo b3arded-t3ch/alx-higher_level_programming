675ecd90a16fe714df3467076958a942c3f9cbb0
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2024-01-10 17:14:47 +0100

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary != None:
+    if a_dictionary is not None:
         val_u = []
         for value in a_dictionary.values():
             val_u.append(value)
         val_u.sort()
         big_gest = val_u[-1]
         for key, value in a_dictionary.items():
-            if a_dictionary[key] == None:
+            if a_dictionary[key] is None:
                 return (None)
-            elif a_dictionary[key] == big_gest:
+            elif a_dictionary[key] is big_gest:
                 return (key)
