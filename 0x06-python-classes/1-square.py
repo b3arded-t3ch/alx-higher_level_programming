9b37ee6ae07cc1ff9b8476c91fae4681879ba014
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2024-01-23 15:45:59 +0100

    initiating class Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""This defines a class Square"""
+
+
+class Square:
+    """This represeents a class with size"""
+
+    def __init__(self, size):
+        """The __init__ method initialises the attribute, size"""
+        self.__size = size
