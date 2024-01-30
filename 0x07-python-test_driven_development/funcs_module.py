6b0690fe4e98d9b1b0711d4fa9ac85fb3a19f48e
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2024-01-30 23:22:34 +0100

    tdd

---
+++ 0x07-python-test_driven_development/funcs_module.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+
+This module contains functions used in python TDD exercise.
+
+"""
+
+def add_integer(a, b=98):
+    """This func adds two integers
+
+    Args:
+        a(int): the first operand.
+        b(int): the second operand.
+
+    Returns:
+        int: the sum of a and b.
+
+    """
+    return a + b
+
