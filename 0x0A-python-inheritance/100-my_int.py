25ee889d81633b4726929a2f66cf2a3596525147
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2024-02-05 23:23:42 +0100

    rebel

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -2,13 +2,13 @@
 """This class is a rebel!"""
 
 
-class MyInt(Int):
+class MyInt(int):
     """The class rebels the standard behaviour of equality signs"""
 
     def __eq__(self, value):
         """turn equals to upside down"""
-        return super().__eq__(value) != value
+        return self.real != value
 
     def __ne__(self, value):
         """turn not equals to upside down"""
-        return super().__ne__(value) == value
+        return self.real == value
