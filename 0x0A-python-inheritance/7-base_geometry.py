53ea347b03c70167f773b8f216d20ff7f5d933b7
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2024-02-05 17:29:54 +0100

    adjusting base geometry if case

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,10 +7,18 @@ class BaseGeometry:
     def area(self):
         """Raises a no-implementation Exception"""
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
-        """validates an integer"""
-        if type(value) != int:
+        """validates an integer
+            Args:
+                name (str): The name of the parameter.
+                value (int): The parameter to validate.
+            Raises:
+                TypeError: If value is not an integer.
+                ValueError: If value is <= 0.
+
+        """
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
