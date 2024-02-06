ae7adf8a63d0b08f12dad8b065a3662047f00e8e
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2024-02-06 23:32:25 +0100

    append after

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -10,12 +10,11 @@ def append_after(filename="", search_string="", new_string=""):
         search_string (str): The string to search for within the file.
         new_string (str): The string to insert.
     """
+    modified_content = ""
     with open(filename, mode='r', encoding="utf-8") as f:
-        lines = f.readlines()
-        modified_content = ""
-        for line in lines:
+        for line in f:
+            modified_content += line
             if search_string in line:
-                line += new_string
-                modified_content += line
+                modified_content += new_string
     with open(filename, mode='w', encoding="utf-8") as n:
         n.write(modified_content)
