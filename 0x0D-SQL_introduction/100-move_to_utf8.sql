62e35b6393540f46cc80efb95099ce1d8f6ba316
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2024-03-12 22:30:50 +0100

    temperatures.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,16 +1,4 @@
 -- converts hbtn_0c_0 database to UTF8 in my MySQL server.
-ALTER DATABASE
-	hbtn_0c_0
-	CHARACTER SET = utf8mb4
-	COLLATE = utf8mb4_unicode_ci;
-
-ALTER TABLE
-	first_table
-	CONVERT TO CHARACTER SET utf8mb4
-	COLLATE = utf8mb4_unicode_ci;
-
-ALTER TABLE
-	first_table
-	CHANGE `name`
-	CHARACTER SET = utf8mb4
-	COLLATE = utf8mb4_unicode_ci;
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
