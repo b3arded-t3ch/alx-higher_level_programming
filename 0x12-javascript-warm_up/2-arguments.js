bf421343b2371a1916198281d49b32bbd5244f95
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2024-04-09 11:27:54 +0100

    fixed code

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,7 @@
-#!/usr/bin/env node
-const args = process.argv.slice(2);
-
-if (args.length === 0) {
+#!/usr/bin/node
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (args.length === 1) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
