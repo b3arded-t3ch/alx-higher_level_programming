8f235d577e6bd43af8f44cb34d87c4ab3556afbd
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2024-12-17 11:07:52 +0100

    writing first script

--- 0x12-javascript-warm_up/103-object_fct.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const myObject = {
-	type: 'object'
-	value: 12
-};
-console.log(myObject);
-function myFunc () {
-	return ++myObject.value;
-};
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-args.forEach(arg => {
-  console.log(arg);
-});
